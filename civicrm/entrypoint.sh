5d9f9230864f01b1d54e7cbb65c60d59c1c3da72
Timo Kramer <fw-git@timokramer.de>
2020-01-23 18:17:12 +0100

    tmp not working

--- civicrm/entrypoint.sh
+++ civicrm/entrypoint.sh
@@ -15,28 +15,26 @@ CIVICRM_VERSION=5.21.1
 set -eux;
 
 if [[ ! -e /var/www/html/index.php ]]; then
-    cd /tmp
     # downloading drupal
     curl -fSL "https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o drupal.tar.gz
     echo "${DRUPAL_MD5} *drupal.tar.gz" | md5sum -c -
     tar -xz --strip-components=1 -f drupal.tar.gz --directory /var/www/html/
+    rm drupal.tar.gz
     # downloading drupal german language
     curl --location https://ftp.drupal.org/files/translations/${DRUPAL_VERSION:0:1}.x/drupal/drupal-${DRUPAL_VERSION}.de.po --output /var/www/html/profiles/standard/translations/drupal-${DRUPAL_VERSION}.de.po
     # downloading civicrm
     curl --location https://download.civicrm.org/civicrm-${CIVICRM_VERSION}-drupal.tar.gz --output civicrm.tar.gz
     tar -xzf civicrm.tar.gz --directory /var/www/html/sites/all/modules/
+    rm civicrm.tar.gz
     # downloading civicrm german language
     curl --location https://download.civicrm.org/civicrm-${CIVICRM_VERSION}-l10n.tar.gz --output civicrm-l10n.tar.gz
     tar -xzf civicrm-l10n.tar.gz civicrm/l10n --strip-components 1
     mv l10n/de_DE /var/www/html/sites/all/modules/civicrm/l10n/
-    # clean
-    rm -rf /tmp/*
+    rm civicrm-l10n.tar.gz
     # permissions
     cd /var/www/html/
     chmod ug+w sites/default
     chown -R 1001:1000 sites modules themes
-    # config file
-    cp ./sites/default/default.settings.php ./sites/default/settings.php
 fi
 # start php-fpm
 php-fpm
